3701e5490145f69b5a72b963a6ffdc49bb10d5d3
Simone Campagna <scampagna@softray.it>
2016-09-20 17:28:51 +0200

    period.month_picker: add $rootScope dependency AND add listeners for month changes AND jsDoc function description

--- app/components/period/directives/month_picker/month_picker-directive.js
+++ app/components/period/directives/month_picker/month_picker-directive.js
@@ -4,8 +4,8 @@
 	angular.module("wtd")
 		.directive("monthPicker", monthPicker);
 
-	monthPicker.$inject = ['loggerService', 'monthTranslationUtilityService', 'integrityCheckService'];
-	function monthPicker(logger, mtus, ics) {
+	monthPicker.$inject = ['$rootScope', 'loggerService', 'monthTranslationUtilityService', 'integrityCheckService'];
+	function monthPicker(root, logger, mtus, ics) {
 		return {
 			restrict: 'E',
 			replace: true,
@@ -30,34 +30,66 @@
 		}
 	};
 
-	MonthPickerController.$inject = ['$scope', 'loggerService', 'monthTranslationUtilityService'];
-	function MonthPickerController(scope, logger, mtus) {
-		logger.log(this, "start controller");
+	MonthPickerController.$inject = ['$rootScope', '$scope', 'loggerService', 'monthTranslationUtilityService'];
+	function MonthPickerController(root, scope, logger, mtus) {
+		logger.log(this.__proto__.constructor.name, "load");
+		
 		scope.nextMonth = nextMonth;
 		scope.previousMonth = previousMonth;
 
+		/**
+		 * Skip to next month number
+		 * @fires Date#nextMonth
+		 */
 		function nextMonth() {
-			if(scope.month < 11)
+			if(scope.month < 11) {
 				scope.month++;
-		}
-
-		function previousMonth() {
-			if(scope.month > 0)
-				scope.month--;
-		}
-
-		scope.$watch(
-			function() {
-				return scope.month;
-			}, function() {
-				scope.monthName = mtus.getMonthFullName(scope.month);
-				console.log("month changed");
+				root.$broadcast("Date#nextMonth", scope.month);
 			}
-		);
+		}
 
+		/**
+		 * Skip to previous month number
+		 * @fires Date#previousMonth
+		 */
+		function previousMonth() {
+			if(scope.month > 0) {
+				scope.month--;
+				root.$broadcast("Date#previousMonth", scope.month);
+			}
+		}
+
+		/**
+		 * Update current month's name to selected month
+		 * @param month new month selected
+         */
+		function updateCurrentMonthName(month) {
+			scope.month = month;
+			scope.monthName = mtus.getMonthFullName(scope.month);
+		}
+
+		/**
+		 * Update the month's name when month is changed
+		 * @listen Date#previousMonth
+         */
+		root.$on("Date#previousMonth", function(month) {
+			updateCurrentMonthName(month);
+		});
+
+		/**
+		 * Update the month's name when month is changed
+		 * @listen Date#nextMonth
+         */
+		root.$on("Date#nextMonth", function(month) {
+			updateCurrentMonthName(month);
+		});
+
+		/**
+		 * Update the month's name when month is changed
+		 * @listen Date#monthChanged
+         */
+		root.$on("Date#monthChanged", function(month) {
+			updateCurrentMonthName(month);
+		});
 	}
-
-
-
-
 })();
