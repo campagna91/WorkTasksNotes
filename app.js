3aa24e1ec6f53f807d343aed41b0a79edadd0e6c
Simone Campagna <scampagna@softray.it>
2016-10-07 10:15:00 +0200

    [Node] - Add app.js configuration file

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-(function() {
-	angular.module('wtd', []);
-})();
+var express = require('express');
+var app = express();
+
+// Setting public content
+app.use(express.static('public'));
+
+// Port setting
+app.listen(5050, function () {
+	console.log('Setting listening port on 5050!');
+});
