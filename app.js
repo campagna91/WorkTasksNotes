b325b7a60dfd1fbe354aa372071207bb509996d1
Simone Campagna <scampagna@softray.it>
2016-10-07 17:59:06 +0200

    [app.js] - Add moment and lovefield static references

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 // Setting public content
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+// Expose internal lovefield ORM
+app.use("/model", express.static(__dirname + '/node_modules/lovefield/dist/'));
+
+// Expose internal moment time library
+app.use("/utilities", express.static(__dirname + '/node_modules/moment/'));
 
 // Port setting
 app.listen(5050, function () {
